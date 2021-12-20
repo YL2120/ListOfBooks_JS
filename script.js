d75e6193eab8a7f7571e70ee2bd92e66961189a9
Yassine Layachi <yassinelayachi@outlook.com>
2021-12-20 10:04:08 +0100

    list of books finished

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const bookList = document.querySelector(".book-list");
 const bookForm = document.querySelector(".book-form");
+const container = document.querySelector(".container");
 
 class Book {
     constructor(title, author, year){
@@ -20,6 +21,31 @@ class Book {
         bookList.appendChild(row); //we add our row to the list
     }
 
+    clearFields(){
+        document.getElementById("title").value = "";
+        document.getElementById("author").value = "";
+        document.getElementById("year").value = "";
+    }
+
+    showAlert(message,className){
+        const alert = document.createElement("div"); // on crée une div 
+        alert.className = `alert ${className}`; //ajout nom de la classe à la div
+        alert.appendChild(document.createTextNode(message)); //"createTextnode" : va créér un noeud dans le DOM. Ici on ajoute à la div du texte à l'intérieur
+        container.insertBefore(alert, bookForm); // finalement, on ajoute notre div à container
+
+        setTimeout(() => {
+            document.querySelector('.alert').remove(); // on sélectionne l'élément avec le nom de classe alert et on le supprime
+        },2500)
+    }
+
+}
+
+class Interface{ //classe utilitaire où il n y a que des méthodes
+    deleteBook(target){
+        if(target.className === "delete"){ //si l'élément que je viens de cliquer a la classe "delete"
+            target.parentElement.parentElement.remove(); // alors on supprime son grand-père : td est le parent, tr est le grand père
+        }
+    }
 }
 
 bookForm.addEventListener('submit', (e) => { 
@@ -30,5 +56,17 @@ bookForm.addEventListener('submit', (e) => {
 
     const book = new Book(title,author,year); // on crée un nouvel objet book.
 
-    book.addBookToList(book); // we add the book that we've just created
+    if(title === "" || author === "" || year === "" ){
+        book.showAlert('The fields cannot be empty !', "error");
+    } else{
+        book.addBookToList(book); // we add the book that we've just created
+        book.clearFields(); //les inputs redeviennent vides.
+        book.showAlert('Book successfully added', "success");
+    }
+    
+})
+
+bookList.addEventListener("click", (e) => {
+    const ui = new Interface();
+    ui.deleteBook(e.target);
 })
